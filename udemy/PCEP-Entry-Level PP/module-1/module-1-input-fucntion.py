947632b47221d4fda5eb1e15d476a6c480106130
Mohamed Moez MANSOURI <nmansouri@externe.europ-assistance.com>
2023-09-22 10:59:38 +0200

    Finishing module 1 and starting module 2

---
+++ udemy/PCEP-Entry-Level PP/module-1/module-1-input-fucntion.py
@@ -0,0 +1,9 @@
+print('What is your name')
+user_name = input('What is your name')
+print('Hello',user_name)
+
+
+login = input('Enter your login:')
+native_language = input('Enter your native language')
+
+print('Your login is',login,'and you speak',native_language)
